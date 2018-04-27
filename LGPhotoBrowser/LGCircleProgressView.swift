ce0046bbb2f89e67730fe2daa0c1971e1044e6f8
龚杰洪 <470046428@qq.com>
2018-04-27 19:21:26 +0800

    完成现有更改

--- LGPhotoBrowser/LGCircleProgressView.swift
+++ LGPhotoBrowser/LGCircleProgressView.swift
@@ -211,3 +211,20 @@ open class LGCircleProgressView: UIView {
     }
 
 }
+
+class LGImageWeakTarget: NSObject {
+    public weak var target: NSObjectProtocol?
+    public init(target: NSObjectProtocol) {
+        super.init()
+        self.target = target
+    }
+    
+    public override func responds(to aSelector: Selector!) -> Bool {
+        return (target?.responds(to: aSelector) ?? false) || super.responds(to: aSelector)
+    }
+    
+    public override func forwardingTarget(for aSelector: Selector!) -> Any? {
+        return target
+    }
+}
+
